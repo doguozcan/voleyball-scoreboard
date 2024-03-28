d3bc724948d5b9cdce36ac4e76f71659d2cbd5e8
doguozcan <76822280+doguozcan@users.noreply.github.com>
2024-03-28 14:41:58 +0300

    Implement update to the counter slice

--- src/features/counter/counterSlice.js
+++ src/features/counter/counterSlice.js
@@ -3,17 +3,45 @@ import { createSlice } from '@reduxjs/toolkit'
 export const counterSlice = createSlice({
   name: 'counter',
   initialState: {
-    value: 0,
+    teamASetScore: 0,
+    teamAMatchScore: 0,
+    teamBSetScore: 0,
+    teamBMatchScore: 0,
   },
   reducers: {
-    increment: (state) => {
-      state.value += 1
+    incrementASetScore: (state) => {
+      state.teamASetScore += 1
     },
-    decrement: (state) => {
-      state.value -= 1
+    incrementBSetScore: (state) => {
+      state.teamBSetScore += 1
+    },
+    decrementASetScore: (state) => {
+      state.teamASetScore -= 1
+    },
+    decrementBSetScore: (state) => {
+      state.teamBSetScore -= 1
+    },
+    incrementAMatchScore: (state) => {
+      state.teamAMatchScore += 1
+    },
+    incrementBMatchScore: (state) => {
+      state.teamBMatchScore += 1
     },
   },
 })
 
-export const { increment, decrement } = counterSlice.actions
+export const aSetScore = (state) => state.counter.teamASetScore
+export const bSetScore = (state) => state.counter.teamBSetScore
+export const aMatchScore = (state) => state.counter.teamAMatchScore
+export const bMatchScore = (state) => state.counter.teamBMatchScore
+
+export const {
+  incrementASetScore,
+  incrementBSetScore,
+  decrementASetScore,
+  decrementBSetScore,
+  incrementAMatchScore,
+  incrementBMatchScore,
+} = counterSlice.actions
+
 export default counterSlice.reducer
